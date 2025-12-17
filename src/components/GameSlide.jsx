dc9fc91e6978cf0d2e119f0c6d3f35c6f5d435cf
bnrpmbd <banarpambudi094@gmail.com>
2025-12-17 19:17:43 +0700

    Fix SwiperSlide

--- src/components/GameSlide.jsx
+++ src/components/GameSlide.jsx
@@ -1,43 +1,39 @@
 import React from 'react'
-// Import Swiper React components
-import { SwiperSlide } from 'swiper/react';
 
 function GameSlide({game, active, toggleVideo}) {
   return (
-    <SwiperSlide>
-        <div className="gameSlider">
-            <img src={game.img} alt={game.title} />
-            <div className={`video ${active ? 'active' : undefined}`}>
-                <iframe 
-                    width="1280"
-                    height="720"
-                    src={game.trailer}
-                    title={game.title}
-                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-                    allowFullScreen
-                ></iframe>
-            </div>
-            <div className="content">
-                <h2>{game.title}</h2>
-                <p>{game.description}</p>
-                <div className="buttons">
-                    <button type="button" className="orderBtn">Order Now</button>
-                    <button 
-                        type="button" 
-                        className={`playBtn ${active ? 'active' : undefined}`} 
-                        onClick={toggleVideo}
-                    >
-                        <span className="pause">
-                            <i className="bi bi-pause-fill"></i>
-                        </span>
-                        <span className="play">
-                            <i className="bi bi-play-fill"></i>
-                        </span>
-                    </button>
-                </div>
+    <div className="gameSlider">
+        <img src={game.img} alt={game.title} />
+        <div className={`video ${active ? 'active' : undefined}`}>
+            <iframe 
+                width="1280"
+                height="720"
+                src={game.trailer}
+                title={game.title}
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+                allowFullScreen
+            ></iframe>
+        </div>
+        <div className="content">
+            <h2>{game.title}</h2>
+            <p>{game.description}</p>
+            <div className="buttons">
+                <button type="button" className="orderBtn">Order Now</button>
+                <button 
+                    type="button" 
+                    className={`playBtn ${active ? 'active' : undefined}`} 
+                    onClick={toggleVideo}
+                >
+                    <span className="pause">
+                        <i className="bi bi-pause-fill"></i>
+                    </span>
+                    <span className="play">
+                        <i className="bi bi-play-fill"></i>
+                    </span>
+                </button>
             </div>
         </div>
-    </SwiperSlide>
+    </div>
   )
 }
 
