a20dce442edef751372f158be17118bd70ce1e60
bnrpmbd <banarpambudi094@gmail.com>
2025-12-17 19:09:36 +0700

    Update Fetch Data

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
     }
 
     const fetchData = () => {
-        fetch('http://localhost:3000/api/gamesData.json')
+        fetch('/api/gamesData.json')
             .then(res => res.json())
             .then(data => {
                 setGames(data);
